8d57660c52757ef11d90794ca28e03932e157f4d
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-13 14:38:29 -0500

    I have completed step one.

---
+++ todo/reducers/reducer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const initialState = {
+    todo: 
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    }
+  };
+
+const reducer = (state, action) => { 
+    return {};
+}
+
+export default initialState;
+export default reducer;
